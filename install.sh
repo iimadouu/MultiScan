f3e1d4006a6ffa2afaecabfe29371de40b790217
Imad eddine Aris <93334453+iimadouu@users.noreply.github.com>
2023-06-21 18:35:59 +0100

    Create install.sh
    first commit

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Install required packages
+
+sudo apt-get update
+
+sudo apt-get install nmap
+
+sudo apt-get install sqlmap
+
+sudo apt-get install commix
+
+sudo apt-get install xsser
+
+sudo apt-get install nikto
+
+sudo apt-get install dirb
+
+sudo apt-get install wfuzz
+
+sudo apt-get install sslscan
+
+sudo apt-get install wpscan
+
+sudo apt-get install sslyze
+
+sudo apt-get install skipfish
+
+sudo apt-get install wapiti
+
+sudo apt-get install subfinder
+
+sudo apt-get install whatweb
+
+sudo apt-get install python
+
+sudo apt-get install python3
+
+sudo apt-get install python3-pip
+
+sudo apt-get install ruby
+
+sudo apt-get install clang
+
+sudo apt-get update
+
+sudo apt-get upgrade
